e209f3485cae060f5f92b4290501bb22b64badad
Daniel0713S <144562452+Daniel0713S@users.noreply.github.com>
2023-12-15 22:47:26 -0800

    add comment

--- Exercise/exercise3b/Serialize.java
+++ Exercise/exercise3b/Serialize.java
@@ -1,13 +1,14 @@
 import java.io.*;
 
-
+// Driver class for serializing and deserializing Shape object
 public class Serialize {
     public static void main(String[] args){
         Shape triangle = new triangle(3, 5, "triangle");
         Shape rectangle = new rectangle(3, 5, "rectangle");
         Shape circle = new circle(3, "circle");
         Shape square = new square(3, "square");
-
+        
+// Serialize and deserialize each shape
         sANDd(triangle);
         sANDd(rectangle);
         sANDd(circle);
@@ -16,16 +17,17 @@ public class Serialize {
 }
 
 
-
+// Method to serialize and deserialize a Shape object
     public static void sANDd(Shape shape){
         try{
+            // Serialize the shape
             FileOutputStream fileOut = new FileOutputStream("serialized_shape.ser");
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
             out.writeObject(shape);
             out.close();
             fileOut.close();
 
-
+            // Deserialize the shape
             FileInputStream fileIn = new FileInputStream("serialized_shape.ser");
             ObjectInputStream in = new ObjectInputStream(fileIn);
             Shape deserializedShape = (Shape) in.readObject();
